93bee8ad435f6b248329b92433218635b24b20bd
Your Name <you@example.com>
2023-10-17 11:24:54 +0200

    first commit

---
+++ res/js/main.js
@@ -0,0 +1,49 @@
+/*
+// {} - scope
+function fce() {
+    console.log("Ahoj svete");
+}
+
+// () => - arrow funkce
+const fce2= () => {
+    console.log("Ahoj svete")
+}
+
+//               item - parametr
+function konsel(item){
+    console.log(`Prinesl jsem ${item} `)
+}
+
+
+//                item - parametr
+const konsel2 = (item, item2) => {
+    console.log(`Prinesl jsem ${item} a ${item2}`)
+}
+//              argument
+konsel("piti")
+//              argument
+konsel("urbana")
+//              argument2
+konsel2("zidle", "bobika")
+//              argument2
+konsel2("jidlo", "piti")
+
+*/
+
+function sum(a, b) {
+    console.log(a + b)
+}
+
+let resultOne = sum(6,4);
+let resultTwo = sum(10, 4);
+
+console.log(resultOne+ resultTwo)
+sum(6, 4)
+sum(10, 4)
+sum(2, 4)
+sum(5, 4)
+sum(5, 4)
+sum(8, 4)
+sum(6, 4)
+sum(7, 4)
+sum(3, 4)
